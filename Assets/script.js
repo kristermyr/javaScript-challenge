5dad1b73da3df8b84797c279ed97beaf29a34bb4
kristermyr <krister90@gmail.com>
2022-11-10 11:59:05 -0600

    corrected error prompting error message over 128 characters by reorganizing code

--- Assets/script.js
+++ Assets/script.js
@@ -5,18 +5,9 @@ function generatePassword() {
 
 
     var passwordLength = window.prompt("How many characters do you want in your password?")   // User sets value for passwordLength
-    var userNeedsNumbers = window.confirm("Do you want numbers in your password?")
-    var userNeedsUpperCase = window.confirm("Do you want upper case letters in your password?")  //creates windows for user to respond with boolean value
-    var userNeedsLowerCase = window.confirm("Do you want lower case letters in your password?") 
-    var userNeedsSpecialCharacter = window.confirm("Do you want special characters in your password?")
-    
-    var userSelectNumber = ["0","1","2","3","4","5","6","7","8","9"];        // array of the different options for the user             
-    var userSelectUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    var userSelectLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];     
-    var userSelectSpecialCharacter = ["!","#","$","%","&","'","(","/",")", ",","*","+","-",".","/",":",";","<","=",">","?","@","[","_","`","{","|","}","~","]"]; 
-  
 
-      if (isNaN(passwordLength)) {
+    
+     if (isNaN(passwordLength)) {
       window.alert("That's not a number!")        // Alerts if user input is not a number. If what user inputs is not a number it alerts and  returns to the start of the page  
       return "Try again!";
       }
@@ -25,6 +16,18 @@ function generatePassword() {
       window.alert("Password length must be more than 8 characterts and not more than 128 characters")
       return "Try again!";
       }
+      
+      var userNeedsNumbers = window.confirm("Do you want numbers in your password?")
+      var userNeedsUpperCase = window.confirm("Do you want upper case letters in your password?")  //creates windows for user to respond with boolean value
+      var userNeedsLowerCase = window.confirm("Do you want lower case letters in your password?") 
+      var userNeedsSpecialCharacter = window.confirm("Do you want special characters in your password?")
+
+
+      var userSelectNumber = ["0","1","2","3","4","5","6","7","8","9"];        // array of the different options for the user             
+      var userSelectUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+      var userSelectLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];     
+      var userSelectSpecialCharacter = ["!","#","$","%","&","'","(","/",")", ",","*","+","-",".","/",":",";","<","=",">","?","@","[","_","`","{","|","}","~","]"]; 
+  
 
 
   var userSelect = [];
